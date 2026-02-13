f7061c077840058c572a2c2da3fe929548031e16
LeThinhPhat <lethinhphat7102003@gmail.com>
2026-02-13 16:49:56 +0700

    tesst

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,6 @@
 // app/products/[id]/page.tsx
 import ProductDetailPage from "@/app/components/product/ProductDetailPage";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <ProductDetailPage id={params.id} />;
+export default function Page() {
+  return <ProductDetailPage />;
 }
